cf87b326c1c7dd411e05fd65985114a3b54c416c
paida09 <paidacherry@gmail.com>
2023-07-13 08:09:40 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+  *main - Printing C program that prints exactly a statement
+  *Return 1 (success)
+  */
+int main(void)
+{
+	fput("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19");
+	return (1);
+}
