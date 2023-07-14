c8a5149090c46041b0943ab757bb4f25ce3154af
paida09 <paidacherry@gmail.com>
2023-07-14 00:08:21 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
   *main - Printing C program that prints exactly a statement
-  *Return: 1 (success)
+  *Return: 0 (success)
   */
 int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, ", stdout);
 	fputs("2015 - 10 - 19\n", stdout);
-	return (1);
+	return (0);
 }
