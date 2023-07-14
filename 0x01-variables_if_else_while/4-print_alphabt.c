1cf2cb16da8485ab446db30e3fbbdb8f777ebc93
paida09 <paidacherry@gmail.com>
2023-07-14 06:33:02 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
