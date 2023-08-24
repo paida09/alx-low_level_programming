f045af0065952a95d11676b1eb7fd06d062ae17e
paida09 <paidacherry@gmail.com>
2023-08-24 03:52:45 -0700

    Add node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,7 +16,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *temp = *head;
 	unsigned int len = 0;
 
-	if (str[len])
+	while (str[len])
 		len++;
 
 	start = malloc(sizeof(list_t));
