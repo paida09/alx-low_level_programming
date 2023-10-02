f7ad0c1a1df50d3e7040e674bfdfd5b0f9e37ce2
paida09 <paidacherry@gmail.com>
2023-10-02 05:55:39 -0700

    100-elf_header.c

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,115 @@
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void check_elf(void);
+void print_magic(void);
+void print_class(void);
+void print_data(void);
+void print_version(void);
+void print_abi(void);
+void print_osabi(void);
+void print_type(void);
+void print_entry(void);
+void close_elf(void);
+
+/**
+ * check_elf - ...
+ */
+void check_elf(void)
+{
+    /* Function code */
+}
+
+/**
+ * print_magic - ...
+ */
+void print_magic(void)
+{
+    /* Function code */
+}
+
+/**
+ * print_class - ...
+ */
+void print_class(void)
+{
+    /* Function code */
+}
+
+/**
+ * print_data - ...
+ */
+void print_data(void)
+{
+    /* Function code */
+}
+
+/**
+ * print_version - ...
+ */
+void print_version(void)
+{
+    /* Function code */
+}
+
+/**
+ * print_osabi - Prints the OS/ABI of an ELF header.
+ */
+void print_osabi(void)
+{
+    /* Function code */
+}
+
+/**
+ * print_abi - Prints the ABI version of an ELF header.
+ */
+void print_abi(void)
+{
+    /* Function code */
+}
+
+/**
+ * print_type - ...
+ * @e_type: ...
+ */
+void print_type(void)
+{
+    /* Function code */
+}
+
+/**
+ * print_entry - ...
+ * @e_entry: ...
+ */
+void print_entry(void)
+{
+    /* Function code */
+}
+
+/**
+ * close_elf - ...
+ * @elf: ...
+ */
+void close_elf(void)
+{
+    /* Function code */
+}
+
+/**
+ * main - Displays the information contained in the
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+    /* Function code */
+
+	return (0);
+}
