c0840a96ade3e87c183baa7d878f4612dc19edc6
paida09 <paidacherry@gmail.com>
2023-10-02 03:26:47 -0700

    updated

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- ...
+ * @filename: ...
+ * @letters: ...
+ *
+ * Return: 0 when function fails or filename is NULL.
+ *
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *new;
+	ssize_t fd;
+	ssize_t x;
+	ssize_t y;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	new = malloc(sizeof(char) * letters);
+	y = read(fd, new, letters);
+	x = write(STDOUT_FILENO, new, y);
+
+	free(new);
+	close(fd);
+	return (x);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+my project on 0x15. C - File I/O
